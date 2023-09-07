46b3dbc63dc7ba59db926eeab0646c052baf790d
leeseungmin <xbtmdals7@naver.com>
2023-09-07 10:10:39 +0900

    git 연동

--- javawork/NaverjavaProject/src/bit701_day0907/Ex1_Array.java
+++ javawork/NaverjavaProject/src/bit701_day0907/Ex1_Array.java
@@ -4,7 +4,8 @@ public class Ex1_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("배열 복습 예제");
+		
+		System.out.println("배열 복습 예제입니다");
 	}
 
 }
