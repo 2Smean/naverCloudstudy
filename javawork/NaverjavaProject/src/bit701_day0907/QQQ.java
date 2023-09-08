04b5b86a14cb5473f2679c9ee4af7cea6045b154
leeseungmin <xbtmdals7@naver.com>
2023-09-08 11:00:51 +0900

    로 또 문 제

---
+++ javawork/NaverjavaProject/src/bit701_day0907/QQQ.java
@@ -0,0 +1,13 @@
+package bit701_day0907;
+
+public class QQQ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =5;
+		int y =0;
+		int result =x/y;
+		System.out.println(result);
+	}
+
+}
