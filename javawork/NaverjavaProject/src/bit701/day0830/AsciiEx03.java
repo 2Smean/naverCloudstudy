6bae2699f970bc99aaa623314c3fb39d48219c47
leeseungmin <xbtmdals7@naver.com>
2023-10-06 20:31:43 +0900

    json

--- javawork/NaverjavaProject/src/bit701/day0830/AsciiEx03.java
+++ javawork/NaverjavaProject/src/bit701/day0830/AsciiEx03.java
@@ -10,13 +10,13 @@ public class AsciiEx03 {
 		System.out.println(a1);//A
 		System.out.println(a2);//66
 		System.out.println((int)a1);//65 아스키 코드값으로 출력하고 싶은겨우
-		System.out.println((char)a2);//B
-		
+						System.out.println((char)a2);//B
+
 		System.out.printf("%d 의 아스키문자는 %c 이다\n" ,(int)a1,a1);
-		
+
 		char b1='C';
 		int b2=68;
-		
+
 		System.out.println(b1);
 		System.out.println(b2);
 		System.out.println(b1+2);//묵시적 형 변환에 의해서 char+int=int, 그러므로 E 가 아니라 69가 나온다
@@ -28,13 +28,13 @@ public class AsciiEx03 {
 		 *  String + int = String
 		 *  String + double =String
 		 */
-		
+
 		//print 나 println 은 모든타입 출력이 가능하다
 		//이유는 타입별로 여러개를 같은 이름으로 만들어놨기 때문이다
 		//이런 메서드를 중복함수 (overloding method) 라고 한다
-		
+
 		System.out.println((char)(b1+2));//b1+2=69 로 int 타입인데 char 로 강제 형 변환해서 출력
-		
+
 		System.out.println(5/2);// 2 , int 와 int 의 연산이므로 무조건 결과는 int 타입으로 나온다
 		System.out.println(5.0/2);// double + int = double, 2.5
 		System.out.println((double)5/2);//double + int = double
---
+++ javawork/NaverjavaProject/src/bit701/day0926/OverEx.java
@@ -0,0 +1,48 @@
+package bit701.day0926;
+
+class Shape{
+	public void draw() {
+		System.out.println("Shape");
+	}
+}
+
+class Line extends Shape {
+//	public void draw() {  //상속받았기 때문에 존재하지만 컴파일러에 의해 무시된다 
+//		System.out.println("Shape");
+//	}
+	public void draw(int x) { //오버라이딩
+		
+		System.out.println("Line"); //오버라이딩이 우선된다
+	}
+}
+
+class Circle extends Shape {
+	public void draw() { //오버라이딩
+		System.out.println("Circle");
+	}
+}
+
+class Rect extends Shape {
+	public void draw() { //오버라이딩
+		System.out.println("Rect");
+	}
+}
+
+public class OverEx {
+
+	public static void print(Shape p) { // p는 Shape 객체이거나 Shape을 상속받은 객체에 대한 레퍼런스이다
+		
+	}
+	public static void main(String[] args) {
+		Line line = new Line();
+		Shape shape = line; // 업캐스팅 line 객체를 shape 타입의 레퍼런스가 가리키게 되었다
+		
+		line.draw(); //Line 클래스의 draw() 호출
+		shape.draw(); //Shape 클래스의 draw() 호출 -> 동적 바인딩에 의해 Line 클래스의 draw() 실행
+		
+		print(shape);
+		print();
+		
+	}
+
+}
