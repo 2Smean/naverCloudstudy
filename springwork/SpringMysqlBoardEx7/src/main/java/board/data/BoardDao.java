31d91565b1698bbe96b147cfe3926e65dc8c1c7b
이승민 <136636643+2Smean@users.noreply.github.com>
2023-11-14 17:43:53 +0900

    Spring Mysql DB Connection
    Spring db setting

---
+++ springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
@@ -0,0 +1,19 @@
+package board.data;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository //bean을 자동 등록해주는 Annotation : dao 에 붙인다
+public class BoardDao {
+	
+	@Autowired
+	private SqlSession session;
+	
+	private String nameSpace="board.data.BoardDao.";
+	//전체 갯수 반환하는 메서드 
+	public int getTotalCount()
+	{
+		return session.selectOne(nameSpace+"totalCountOfBoard");
+	}
+}
