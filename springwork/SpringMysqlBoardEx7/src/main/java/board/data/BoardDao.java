16374fb838611b6f8a97693a09d14ebb53cfb660
이승민 <136636643+2Smean@users.noreply.github.com>
2023-11-15 11:54:25 +0900

    spring 231115 part1

--- springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
+++ springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
@@ -1,19 +1,30 @@
 package board.data;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository //bean을 자동 등록해주는 Annotation : dao 에 붙인다
+@Repository // bean을 자동 등록해주는 Annotation : dao 에 붙인다
 public class BoardDao {
-	
+
 	@Autowired
 	private SqlSession session;
+
+	private String nameSpace = "board.data.BoardDao.";
+
+	// 전체 갯수 반환하는 메서드
+	public int getTotalCount() {
+		return session.selectOne(nameSpace + "totalCountOfBoard");
+	}
+
+	public void insertBoard(BoardDto dto) {
+		session.insert(nameSpace + "insertBoard", dto);
+	}
 	
-	private String nameSpace="board.data.BoardDao.";
-	//전체 갯수 반환하는 메서드 
-	public int getTotalCount()
+	public List<BoardDto> getAllDatas()
 	{
-		return session.selectOne(nameSpace+"totalCountOfBoard");
+		return session.selectList(nameSpace+"selectAllBoard");
 	}
 }
