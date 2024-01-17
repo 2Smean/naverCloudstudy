e172e450228b8f530b94bd6f82403416b024b7cf
이승민 <136636643+2Smean@users.noreply.github.com>
2024-01-17 21:24:05 +0900

    spring boot study 20240117 21:24

--- bootwork/MyCarJPABoot/src/main/java/com/example/demo/MyCarJpaBootApplication.java
+++ bootwork/MyCarJPABoot/src/main/java/com/example/demo/MyCarJpaBootApplication.java
@@ -9,9 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import jakarta.persistence.Entity;
 
 @SpringBootApplication
-@ComponentScan({"mycar.*","naver.storage"})
-@EntityScan("mycar.data")
-@EnableJpaRepositories("mycar.repository")
+@ComponentScan({"mycar.*","naver.storage","person.*","guest.*"})
+@EntityScan("*.data")
+@EnableJpaRepositories({"mycar.repository","person.data","guest.data"})
 public class MyCarJpaBootApplication {
 
 	public static void main(String[] args) {
