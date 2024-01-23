67e85af0b5cc20e0240981d8a0a2deefb34e87f5
이승민 <136636643+2Smean@users.noreply.github.com>
2024-01-23 21:04:09 +0900

    react in spring boot study 20240123 21:04

---
+++ bootwork/MybatisBootJwt/src/main/reactjs/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import mainImage1 from '../image/K-041.png';
+import mainImage2 from '../image/K-053.png';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>React + SpringBoot Project</h1>
+            <img alt='' src={mainImage1} style={{ width: '300px', margin: '10px', border: '5px groove orange' }} />
+            <img alt='' src={mainImage2} style={{ width: '300px', margin: '10px', border: '5px groove orange' }} />
+        </div>
+    );
+};
+
+export default Home;
---
+++ bootwork/MybatisBootJwt/src/main/reactjs/src/components/board/BoardList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BoardList = () => {
+    return (
+        <div>
+            <h4 className='alert alert-danger'>보드 리스트</h4>
+        </div>
+    );
+};
+
+export default BoardList;
