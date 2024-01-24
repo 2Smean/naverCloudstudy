f32f785755d80b2e268f95bd626fb5cfa5b554e8
이승민 <136636643+2Smean@users.noreply.github.com>
2024-01-24 18:52:53 +0900

    react in spring boot study 20240124 18:52

--- bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberList.js
+++ bootwork/MybatisBootJwt/src/main/reactjs/src/components/member/MemberList.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import { Alert } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MemberCard from './MemberCard';
 
 const MemberList = () => {
+    const [memberlist, setMemberList] = useState([]);
+
+    const navi = useNavigate();
+
+    const getMemberlist = () => {
+        axios.get("/member/list")
+            .then(res => {
+                console.log(res)
+                setMemberList(res.data.mlist);
+            })
+    }
+
+    useEffect(() => {
+        getMemberlist();
+    }, []);
+
+    //삭제 이벤트 
+    const deleteMember = (num) => {
+        const a = window.confirm("삭제하려면 확인을 눌러주세요!");
+        if (a) {
+            const url = "/member/delete?num=" + num;
+            axios.delete(url)
+                .then(res => {
+                    //삭제 성공 후 목록 다시 출력 
+                    getMemberlist();
+                })
+        }
+    }
+
     return (
         <div>
-            <h4 className='alert alert-danger'>멤버목록</h4>
+            <h4 className='alert alert-danger'>회원 목록(총 {memberlist.length}명)</h4>
+            <table className='table table-bordered'>
+
+                <tbody>
+                    {
+                        memberlist &&
+                        memberlist.map((row, idx) => (
+                            <MemberCard key={idx} row={row}
+                                deleteMember={deleteMember} />
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
